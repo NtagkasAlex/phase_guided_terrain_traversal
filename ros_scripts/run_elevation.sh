9d234f70a13b276272f70cc4eb65135bcf6f9530
NtagkasAlex <up1083874@upnet.gr>
2025-11-30 22:10:50 +0200

    Update deployment pipeline

--- ros_scripts/run_elevation.sh
+++ ros_scripts/run_elevation.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-# Source your ROS 2 setup
-
-
-# Run transform_sensors node
-#cd ~/autonomy_stack_go2
-#source install/setup.bash
-#ros2 run transform_sensors transform_everything &
-#TRANSFORM_PID=$!
-
-# Run point_lio launch file
-
-cd ~/ros_scripts
+cd ~/Desktop/phase_guided_terrain_traversal/ros_scripts
 ./run_point_lio.sh&
 LIO_PID=$!
 
-# Run your Python script
 
-cd ~/elevation
+
+cd ~/Desktop/phase_guided_terrain_traversal/ros_ws
 colcon build --packages-select elevation_mapping grid_map_filter_node --cmake-args -DCMAKE_BUILD_TYPE=Release 
 source install/setup.bash
 # Wait for all processes to finish
