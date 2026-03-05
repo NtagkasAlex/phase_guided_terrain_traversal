3136e01c739f98556d1baea770f360d729e3b11a
NtagkasAlex <up1083874@upnet.gr>
2026-03-05 02:15:45 +0200

    runs indexing

--- training/training.sh
+++ training/training.sh
@@ -2,39 +2,40 @@
 
 ROBOT="${1:-go2}"
 STEPS=200_000_000
+IFS=', ' read -ra RUNS <<< "${RUNS:-0}"
 METHODS=("pgtt" "baseline" "wild")
 LEVELS=("level03" "level07" "level10" "level13")
 
 echo "Training robot: $ROBOT"
 
-for ((run=0; run<1; run++)); do
-    echo "=== Run $((run+1)) ==="
+for RUN in "${RUNS[@]}"; do
+    echo "=== Run $((RUN+1)) ==="
 
-    for method in "${METHODS[@]}"; do
-        prev_ckpt=""
+for method in "${METHODS[@]}"; do
+    prev_ckpt=""
 
-        for level in "${LEVELS[@]}"; do
-            name="${ROBOT}_${method}_${level}_run${run}"
-            rm -rf "checks_stairs/checkpoint_${name}"
+    for level in "${LEVELS[@]}"; do
+        name="${ROBOT}_${method}_${level}_run${RUN}"
+        rm -rf "checks_stairs/checkpoint_${name}"
 
-            if [[ -n "$prev_ckpt" ]]; then
-                python3 -m training.train \
-                    --robot "$ROBOT" \
-                    --method "$method" \
-                    --index "$name" \
-                    --checkpoint_folder "checks_stairs/checkpoint_${prev_ckpt}" \
-                    --terrain_file "terrains/${level}.npy" \
-                    --num_timesteps "$STEPS"
-            else
-                python3 -m training.train \
-                    --robot "$ROBOT" \
-                    --method "$method" \
-                    --index "$name" \
-                    --terrain_file "terrains/${level}.npy" \
-                    --num_timesteps "$STEPS"
-            fi
+        if [[ -n "$prev_ckpt" ]]; then
+            python3 -m training.train \
+                --robot "$ROBOT" \
+                --method "$method" \
+                --index "$name" \
+                --checkpoint_folder "checks_stairs/checkpoint_${prev_ckpt}" \
+                --terrain_file "terrains/${level}.npy" \
+                --num_timesteps "$STEPS"
+        else
+            python3 -m training.train \
+                --robot "$ROBOT" \
+                --method "$method" \
+                --index "$name" \
+                --terrain_file "terrains/${level}.npy" \
+                --num_timesteps "$STEPS"
+        fi
 
-            prev_ckpt="$name"
-        done
+        prev_ckpt="$name"
     done
+done
 done
